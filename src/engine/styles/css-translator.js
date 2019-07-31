13551d7880e719ace4750f3b574d73e48030cb6b
Alex van Vliet <alex.van-vliet@epita.fr>
2019-07-31 16:05:06 +0200

    feat(translator, styles): translate default to css

---
+++ src/engine/styles/css-translator.js
@@ -0,0 +1,20 @@
+function translate(components) {
+  let output = '';
+  for (let component_class in components)
+  {
+    if (components.hasOwnProperty(component_class))
+    {
+      let component = components[component_class];
+
+      if (component.hasOwnProperty('default'))
+      {
+        output += '.' + component_class + '{';
+        output += '@apply ' + component['default'] + ';';
+        output += '}';
+      }
+    }
+  }
+  return output;
+}
+
+export default translate;
---
+++ src/engine/styles/css-translator.test.js
@@ -0,0 +1,29 @@
+import translate from './css-translator';
+
+it('translates the default key of a component correctly', () => {
+  let input = {
+    'my-div': {default: ['text-red-500']},
+  };
+  let output = translate(input);
+  expect(output).toBe(
+      '.my-div{' +
+        '@apply text-red-500;' +
+      '}'
+  );
+});
+
+it('translates several components', () => {
+  let input = {
+    'my-div': {default: ['text-red-500']},
+    'my-nested-div': {default: ['text-red-200']},
+  };
+  let output = translate(input);
+  expect(output).toBe(
+      '.my-div{' +
+        '@apply text-red-500;' +
+      '}' +
+      '.my-nested-div{' +
+        '@apply text-red-200;' +
+      '}'
+  );
+});
