9db31077cb7f50d62f0a6f18580c726481267f70
Alex van Vliet <alex.van-vliet@epita.fr>
2019-07-31 16:44:19 +0200

    feat(view): add editors

--- src/components/app/app-view.js
+++ src/components/app/app-view.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from 'assets/images/logo.svg';
+import React, {useState} from 'react';
+import {Controlled as CodeMirror} from 'react-codemirror2';
+import translate from '../../engine/translator';
+import cssTranslate from '../../engine/styles/css-translator';
+
 import './app.sass';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+
+import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/mode/css/css';
 
 function AppView() {
+  const [data, setData] = useState('');
+
+  const output = translate(data);
+  const style = cssTranslate(output.components);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'App'}>
+      <CodeMirror
+        className={'App__main'}
+        value={data}
+        options={{
+          mode: 'htmlmixed',
+          theme: 'material',
+          lineNumbers: true
+        }}
+        onBeforeChange={(editor, data, value) => {
+          setData(value);
+        }}
+        onChange={(editor, data, value) => {
+        }}
+      />
+      <CodeMirror
+          className={'App__html'}
+          value={output.html}
+          options={{
+            mode: 'htmlmixed',
+            theme: 'material',
+            lineNumbers: true,
+            readOnly: true
+          }}
+      />
+      <CodeMirror
+          className={'App__style'}
+          value={style}
+          options={{
+            mode: 'css',
+            theme: 'material',
+            lineNumbers: true,
+            readOnly: true
+          }}
+      />
     </div>
   );
 }
