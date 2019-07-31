6a8caf3e41a5d9b102aa8f33d4eb0ae83260f99f
Alex van Vliet <alex.van-vliet@epita.fr>
2019-07-31 18:15:32 +0200

    feat(translator, styles): handle sizes and variants

--- src/engine/styles/css-translator.js
+++ src/engine/styles/css-translator.js
@@ -1,3 +1,33 @@
+function handle_variants(component_class, size)
+{
+  let output = "";
+  if (size.hasOwnProperty('_default')) {
+    output += '.' + component_class + '{';
+    output += '@apply ' + size['_default'].join(' ') + ';';
+    output += '}';
+  }
+
+  for (let variant in size)
+  {
+    if (size.hasOwnProperty(variant) && variant !== '_default')
+    {
+      if (variant === 'group-hover')
+      {
+        output += '.group:hover .' + component_class + '{';
+        output += '@apply ' + size[variant].join(' ') + ';';
+        output += '}';
+      }
+      else
+      {
+        output += '.' + component_class + ':' + variant + '{';
+        output += '@apply ' + size[variant].join(' ') + ';';
+        output += '}';
+      }
+    }
+  }
+  return output;
+}
+
 function translate(components) {
   let output = '';
   for (let component_class in components)
@@ -8,12 +38,16 @@ function translate(components) {
 
       if (component.hasOwnProperty('_default'))
       {
-        const size = component['_default'];
-        if (size.hasOwnProperty('_default'))
+        output += handle_variants(component_class, component['_default']);
+      }
+
+      for (let size in component)
+      {
+        if (component.hasOwnProperty(size) && size !== '_default')
         {
-          output += '.' + component_class + '{';
-          output += '@apply ' + size['_default'].join(' ') + ';';
-          output += '}';
+          output += "@screen " + size + "{";
+          output += handle_variants(component_class, component[size]);
+          output += "}";
         }
       }
     }
--- src/engine/styles/css-translator.test.js
+++ src/engine/styles/css-translator.test.js
@@ -40,3 +40,28 @@ it('handles several classes', () => {
   );
 });
 
+it('handles sizes and variants', () => {
+  let input = {
+    'my-div': {lg: {hover: ['text-red-500']}},
+  };
+  let output = translate(input);
+  expect(output).toBe(
+      '@screen lg{' +
+        '.my-div:hover{' +
+          '@apply text-red-500;' +
+        '}' +
+      '}'
+  );
+});
+
+it('handles group-hover', () => {
+  let input = {
+    'my-div': {_default: {'group-hover': ['text-red-500']}},
+  };
+  let output = translate(input);
+  expect(output).toBe(
+      '.group:hover .my-div{' +
+        '@apply text-red-500;' +
+      '}'
+  );
+});
